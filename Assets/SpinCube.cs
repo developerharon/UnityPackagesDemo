5a1d6692668650374dd7432ac178a68da6a82276
Haron Kipkorir <ronoharonkip@gmail.com>
2022-11-04 13:41:56 +0300

    Add project files.

---
+++ Assets/SpinCube.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class SpinCube : MonoBehaviour
+{
+    public Vector3 RotateAmount;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Rotate(RotateAmount);
+    }
+}
